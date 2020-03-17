30c2f4b345edc40d2d3a9c5960d3144a232583b2
Andre Skeene <halodre24@gmail.com>
2020-03-17 19:58:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,9 @@
 require 'Nokogiri'
+require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+create_project_hash
